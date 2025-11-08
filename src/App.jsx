7a6523406ca3aebe49f75a98b6f50630b449ff3e
Omar <jabour.omar@hotmail.com>
2025-11-08 16:04:37 +0100

    useEffect explain  v.02

--- src/App.jsx
+++ src/App.jsx
@@ -17,17 +17,17 @@ export default App
 -------- CYCLE DE VIE D'UN COMPONENT REACT --------
 
 PHASE 1 : MONTAGE (MOUNTING)
-lecture1 :
+lecture1 : render
 lecture2 :
 
 
 PHASE 2 : MISE A JOUR (UPDATING)
-lecture1 :
+lecture1 : render (re-render)
 lecture2 :
 
 
 PHASE 3 : DEMONTAGE (UNMOUNTING)
-lecture1 :
+lecture1 : ❌ (dead)
 lecture2 :
 
 
